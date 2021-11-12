7c1a2769d09fc0fa7d56e1d4860bdcd41305a296
home <home@home-PC>
2021-11-12 12:57:40 +0530

    pranesh edit1

--- src/test/java/TestNg/practice/GitSAmple.java
+++ src/test/java/TestNg/practice/GitSAmple.java
@@ -4,6 +4,7 @@ public class GitSAmple {
 
 	public static void main(String[] args) {
 		System.out.println("This is sample class");
+		System.out.println("this is pranesh edit");
 
 	}
 
---
+++ src/test/java/TestNg/practice/praneshedit.java
@@ -0,0 +1,11 @@
+package TestNg.practice;
+
+public class praneshedit {
+	
+	public void main(String args[])
+	{
+		System.out.println("new edit");
+		System.out.println("hi");
+	}
+
+}
