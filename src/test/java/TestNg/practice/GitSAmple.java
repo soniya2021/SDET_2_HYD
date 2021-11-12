00914c92f471cbc34ff37187476fd997d38ffa98
pvpatil5 <pvpatil123@yahoo.com>
2021-11-12 12:35:09 +0530

    new1

--- src/test/java/TestNg/practice/GitSAmple.java
+++ src/test/java/TestNg/practice/GitSAmple.java
@@ -3,7 +3,7 @@ package TestNg.practice;
 public class GitSAmple {
 
 	public static void main(String[] args) {
-		System.out.println("This is sample class");
+		System.out.println("This is sample hiiii class");
 
 	}
 
