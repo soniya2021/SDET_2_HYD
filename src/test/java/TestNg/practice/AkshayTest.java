e6cb880160f0833bb365ced9d8b24a1f705ae6a6
Sumanth Reddy <varnitharddy21@gmail.com>
2022-02-24 13:49:46 +0530

    SDET2 PROJECT

--- src/test/java/TestNg/practice/AkshayTest.java
+++ src/test/java/TestNg/practice/AkshayTest.java
@@ -4,7 +4,9 @@ public class AkshayTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("AkshayTest");
+//System.out.println("AkshayyyTest");
+
+	}
 	}
 
-}
+
